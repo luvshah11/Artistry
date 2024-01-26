95477f42974e833243e58c126245992df443d4ac
luvshah11 <luv.h.shah@gmail.com>
2024-01-26 14:18:42 -0600

    NavBar
    Navbar with drop down menu, Logo, and Nav links

--- build0/index.js
+++ build0/index.js
@@ -15,3 +15,20 @@ window.onclick = function(event) {
     }
   }
 }
+
+function classToggle() {
+  // const navs = document.querySelectorAll('.nav-links');
+  // console.log(navs);
+  // navs.forEach(nav => nav.classList.toggle('Navbar__ToggleShow'));
+  var x = document.getElementById("nav-links");
+
+  //must match the css display value in style.css ".navbar" under @media
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
+
+// document.querySelector('#toggle-icon')
+//   .addEventListener('click', classToggle);
